9b6448f7c4ecae07345d14e48b8554856eef5c14
Kaue010101 <149123288+Kaue010101@users.noreply.github.com>
2023-10-29 19:49:34 -0300

    Update proj.c

--- proj.c
+++ proj.c
@@ -21,3 +21,25 @@ void copiarString(char destino, const charorigem) {
     }
     destino[i] = '\0';
 }
+// Função para adicionar um novo cliente
+void Novo_Cliente(Cliente clientes[], int numClientes) {
+    if (numClientes < 1000) {
+        Cliente novoCliente;
+        printf("Nome: ");
+        scanf("%s", novoCliente.nome);
+        printf("CPF: ");
+        scanf("%s", novoCliente.cpf);
+        printf("Tipo de conta (comum ou plus): ");
+        scanf("%s", novoCliente.tipo_conta);
+        printf("Valor inicial da conta: ");
+        scanf("%f", &novoCliente.saldo);
+        printf("Senha: ");
+        scanf("%s", novoCliente.senha);
+
+        clientes[numClientes] = novoCliente;
+        (numClientes)++;
+        printf("Cliente adicionado com sucesso.\n");
+    } else {
+        printf("Limite maximo de clientes atingido.\n");
+    }
+}
