76ccfb788ad013278471a24b4e240e0d99a3718e
Kaue010101 <149123288+Kaue010101@users.noreply.github.com>
2023-10-29 19:40:21 -0300

    Update proj.h

--- proj.h
+++ proj.h
@@ -3,6 +3,7 @@
 #ifndef PROJ_H
 #define PROJ_H
 
+// Definição da estrutura Cliente
 typedef struct {
     char nome[50];
     char cpf[12];
@@ -10,12 +11,13 @@ typedef struct {
     float saldo;
     char senha[20];
 } Cliente;
-
+// Definição da estrutura Operacao
 typedef struct {
     char cpf[12];
     char tipo_operacao[20];
     float valor;
 } Operacao;
+
 int compararStrings(const char *str1, const char *str2);
 void copiarString(char destino, const charorigem);
 
