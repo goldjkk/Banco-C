1c2af821eecbe9d6f73a5fa9f5836132842a250d
Matheus Concon <142353640+goldjkk@users.noreply.github.com>
2023-10-29 19:55:47 -0300

    Update proj.c

--- proj.c
+++ proj.c
@@ -64,3 +64,15 @@ void Apagar_Cliente(Cliente clientes[], int numClientes) {
 
     printf("Cliente nao encontrado.\n");
 }
+// Função para listar todos os clientes
+void Listar_clientes(Cliente clientes[], int numClientes) {
+    printf("Lista de Clientes:\n");
+    for (int i = 0; i < numClientes; i++) {
+        printf("Nome: %s\n", clientes[i].nome);
+        printf("CPF: %s\n", clientes[i].cpf);
+        printf("Tipo de conta: %s\n", clientes[i].tipo_conta);
+        printf("Saldo: %.2f\n", clientes[i].saldo);
+        printf("\n");
+    }
+}
+
